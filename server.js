41173188ba028bb4105bfb99848d255a0962f988
Junan Chakma <junan@nimblehq.co>
2020-11-18 12:29:05 +0700

    Use correct project name

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.static('./dist/nimble-survey'));
 
 app.get('/*', function (req, res) {
-  res.sendFile('index.html', { root: 'dist/survey-challenge' });
+  res.sendFile('index.html', { root: 'dist/nimble-survey' });
 });
 
 app.listen(process.env.PORT || 8080);
