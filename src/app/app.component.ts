2a555b94a1e69e432b2f9d40218d321039126148
Junan Chakma <junan@nimblehq.co>
2020-11-24 13:59:41 +0700

    Implemented API service call

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {ApiService} from './services/api.service';
+import {environment} from "../environments/environment";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'nimble-survey';
+
+
+  constructor(private service: ApiService) {
+  }
+
+  ngOnInit() { // misspelled
+    const data = {
+      grant_type: 'password',
+      email: 'junan.cs@gmail.com',
+      password: '12345678',
+    };
+
+    this.service.signIn('api/v1/oauth/token', data).subscribe(res => console.log(res));
+    console.log('The component is initialized');
+  }
 }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {Observable, throwError } from 'rxjs';
+import {Observable, throwError} from 'rxjs';
 import {HttpClient} from '@angular/common/http';
 import {retry, catchError} from 'rxjs/operators';
 import {environment} from './../../environments/environment';
@@ -10,19 +10,17 @@ import {environment} from './../../environments/environment';
 export class ApiService {
     headers: object = {
         'Content-Type': 'application/json',
-        Accept: 'application/json',
-        'Access-Control-Allow-Headers': 'Content-Type',
     };
 
     constructor(private http: HttpClient) {
     }
 
-    public signIn(endpoint: string, option: object): Observable<any> {
-        const apiUrl = `${environment.apiBaseUrl}/api/v1/oauth/token`;
+    public signIn(endpoint: string, params: any): Observable<any> {
+        const apiUrl = `${environment.apiBaseUrl}/${endpoint}`;
         const data = {
             grant_type: 'password',
-            email: 'junan.cs@gmail.com',
-            password: '12345678',
+            email: params.email,
+            password: params.password,
             client_id: environment.apiClientId,
             client_secret: environment.apiClientSecret
         };
@@ -32,20 +30,20 @@ export class ApiService {
 
 
     private postRequest(endpoint: string, data: object): Observable<any> {
-        return this.http.post<any>(endpoint, JSON.stringify(data), this.headers).pipe(
+        return this.http.post<any>(endpoint, data, this.headers).pipe(
             retry(1), catchError(this.handleError)
         );
     }
 
     // Error handling
-    handleError(error: any) {
+    private handleError(error: any) {
         let errorMessage = '';
         if (error.error instanceof ErrorEvent) {
             // Get client-side error
             errorMessage = error.error.message;
         } else {
             // Get server-side error
-            errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+            errorMessage = `Error Code: ${error.status}\nMessage: ${error.message} \n ${error}`;
         }
         console.log(errorMessage);
         return throwError(errorMessage);
