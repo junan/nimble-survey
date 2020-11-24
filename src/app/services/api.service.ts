efccba78074f787d0723b1d07104bb7b8db9eec7
Junan Chakma <junan@nimblehq.co>
2020-11-24 14:09:16 +0700

    Removed warning

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -36,7 +36,7 @@ export class ApiService {
     }
 
     // Error handling
-    private handleError(error: any) {
+    private handleError(error: any): void {
         let errorMessage = '';
         if (error.error instanceof ErrorEvent) {
             // Get client-side error
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,4 @@
 
 export const environment = {
-   production: true,
-   apiBaseUrl: 'https://nimble-survey-web-staging.herokuapp.com/',
-   apiClientId: 'zWRM1gjQrQWu0ohAUyS3pn6Bf5oq4two87B-9JWBc3E',
-   apiClientSecret: 'zWRM1gjQrQWu0ohAUyS3pn6Bf5oq4two87B-9JWBc3E'
+  production: true
 };
