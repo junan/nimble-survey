2fb5630b5adadaad00070f8fa3b686f23b859983
Junan Chakma <junan@nimblehq.co>
2020-11-26 18:48:38 +0700

    Changed the text

--- karma.conf.js
+++ karma.conf.js
@@ -13,8 +13,7 @@ module.exports = function (config) {
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
-      clearContext: false, // leave Jasmine Spec Runner output visible in browser
-      captureConsole: true // To support console.log() in the test. It is helpful for debugging
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageReporter: {
       dir: require('path').join(__dirname, './coverage/nimble-survey'),
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,7 +20,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it('has the title as "nimble-survey"', () => {
+  it('shows the title as "nimble-survey"', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('nimble-survey');
