87a87a6e996b29a5479ea0bafa4746567a34ce56
Junan Chakma <junan@nimblehq.co>
2020-11-26 11:23:25 +0700

    Deserialize API response

--- src/app/services/base.service.ts
+++ src/app/services/base.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
 import { retry, catchError } from 'rxjs/operators';
+import { Deserializer } from 'ts-jsonapi';
 
 @Injectable()
 
@@ -10,13 +12,15 @@ export abstract class BaseService {
     'Content-Type': 'application/json',
   };
 
+  deserializer = new Deserializer({
+    keyForAttribute: 'camelCase'
+  });
+
   constructor(private http: HttpClient) {
   }
 
   postRequest(endpoint: string, data: object): Observable<any> {
-    return this.http.post<any>(endpoint, data, this.headers).pipe(
-      retry(1), catchError(this.handleError)
-    );
+    return this.http.post<any>(endpoint, data, this.headers).pipe( retry(1), catchError(this.handleError), map(data => this.deserialize(data)));
   }
 
   handleError(error: any): Observable<any> {
@@ -28,7 +32,10 @@ export abstract class BaseService {
       // Get server-side error
       errorMessage = `Error Code: ${error.status}\nMessage: ${error.message} \n ${error}`;
     }
-    console.log(errorMessage);
     return throwError(errorMessage);
+  }
+
+  deserialize(data: any): Observable<any> {
+    return this.deserializer.deserialize(data);
   }
 }
