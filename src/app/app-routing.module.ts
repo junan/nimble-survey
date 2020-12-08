386fc39e7e44b26610962a596ecde52cc57897f6
Junan Chakma <junan@nimblehq.co>
2020-12-08 09:32:44 +0700

    Added lazy loading and pre loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
 const routes: Routes = [
+  // Lazy Loading auth modules, loading it only when the use navigates to the '/auth' route.
   { path: 'auth', loadChildren: () => import(`./modules/auth/auth.module`).then(m => m.AuthModule) }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,
+    {
+      // This will tell Angular to fetch all the lazy-loaded modules in the background
+      // after the first rendering of the app was complete.
+      preloadingStrategy: PreloadAllModules
+    })],
   exports: [RouterModule]
 })
 export class AppRoutingModule {
