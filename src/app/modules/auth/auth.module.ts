ea34d013c151a3f94494369b6b287f0e28a9433b
Junan Chakma <junan@nimblehq.co>
2020-12-08 10:43:01 +0700

    Configured stylePreprocessorOptions

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { FormSignInComponent } from '../auth/components/form-sign-in/form-sign-in.component';
+import { HeaderComponent } from '../auth/components/header/header.component';
 import { SignInComponent } from '../auth/pages/sign-in/sign-in.component';
 
 const routes: Routes = [
@@ -9,7 +10,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  declarations: [FormSignInComponent, SignInComponent],
+  declarations: [HeaderComponent, FormSignInComponent, SignInComponent],
   imports: [
     CommonModule,
     RouterModule.forChild(routes)
