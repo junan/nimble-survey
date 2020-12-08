39c0bbbc2de8101bd1f426013bb5025fed815216
Junan Chakma <junan@nimblehq.co>
2020-12-08 10:56:38 +0700

    Fixed path

--- src/app/shared/services/authentication/authentication.service.spec.ts
+++ src/app/shared/services/authentication/authentication.service.spec.ts
@@ -1,7 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { AuthenticationService } from './../authentication/authentication.service';
-import { environment } from './../../../environments/environment';
+import { environment } from './../../../../environments/environment';
 
 describe('ApiService', () => {
   let service: AuthenticationService;
