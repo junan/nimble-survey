880c8091205adce8c69141461a4a2aeecd36dd9f
Junan Chakma <junan@nimblehq.co>
2020-12-22 10:38:13 +0700

    Added auth guard spec

--- src/app/shared/services/guard/signed-in-auth-guard.service.spec.ts
+++ src/app/shared/services/guard/signed-in-auth-guard.service.spec.ts
@@ -1,19 +1,42 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-
+import { SessionService } from '@service/session/session.service';
 import { SignedInAuthGuardService } from './signed-in-auth-guard.service';
 
-describe('SignedInuthGuardService', () => {
-  let service: SignedInAuthGuardService;
+describe('SignedInAuthGuardService', () => {
+  let guardService: SignedInAuthGuardService;
+  let sessionService: SessionService;
+
+  const routeMock: any = { snapshot: {}};
+  const routeStateMock: any = { snapshot: {}, url: '/auth/sign-in'};
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [RouterTestingModule]
     });
-    service = TestBed.inject(SignedInAuthGuardService);
+    guardService = TestBed.inject(SignedInAuthGuardService);
+    sessionService = TestBed.inject(SessionService);
   });
 
-  it('creates the component', () => {
-    expect(service).toBeTruthy();
+  it('creates SignedInAuthGuardService', () => {
+    expect(guardService).toBeTruthy();
+  });
+
+  describe('#canActivate', () => {
+    describe('Given the user is signed in', () => {
+      it('returns false', () => {
+        spyOn(sessionService, 'signedIn').and.returnValue(true);
+
+        expect(guardService.canActivate(routeMock, routeStateMock)).toEqual(false);
+      });
+    });
+
+    describe('Given the user is NOT signed in', () => {
+      it('returns true', () => {
+        spyOn(sessionService, 'signedIn').and.returnValue(false);
+
+        expect(guardService.canActivate(routeMock, routeStateMock)).toEqual(true);
+      });
+    });
   });
 });
