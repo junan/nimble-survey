073cc3953b9f355235f5f2c43281787eae646325
Junan Chakma <junan@nimblehq.co>
2020-12-24 14:42:43 +0700

    Added spec for form forgot component

--- src/app/modules/auth/components/form-forgot-password/form-forgot-password.component.spec.ts
+++ src/app/modules/auth/components/form-forgot-password/form-forgot-password.component.spec.ts
@@ -4,6 +4,12 @@ import { FormForgotPasswordComponent } from './form-forgot-password.component';
 describe('FormForgotPasswordComponent', () => {
   let component: FormForgotPasswordComponent;
   let fixture: ComponentFixture<FormForgotPasswordComponent>;
+  let baseElement: any;
+
+  const SELECTORS = {
+    emailField: '.form-forgot-password input[type="email"]',
+    submitButton: '.form-forgot-password button[type="submit"]',
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -14,10 +20,16 @@ describe('FormForgotPasswordComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(FormForgotPasswordComponent);
     component = fixture.componentInstance;
+    baseElement = fixture.nativeElement;
     fixture.detectChanges();
   });
 
   it('renders the component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('renders forgot form', () => {
+    expect(baseElement.querySelector(SELECTORS.emailField)).toBeTruthy();
+    expect(baseElement.querySelector(SELECTORS.submitButton)).toBeTruthy();
   });
 });
