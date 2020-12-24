7e5795482ca85307a77dc680ff74cf2a9551de48
Junan Chakma <junan@nimblehq.co>
2020-12-24 13:21:34 +0700

    Added mlre style

--- src/app/modules/auth/components/form-forgot-password/form-forgot-password.component.ts
+++ src/app/modules/auth/components/form-forgot-password/form-forgot-password.component.ts
@@ -6,9 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form-forgot-password.component.scss'],
 })
 export class FormForgotPasswordComponent implements OnInit {
-  alertIcon = 'notification';
-  alertTitle = 'Check your email.';
-  alertMessage = 'We’ve email you instructions to reset your password.';
+  alertIcon = '';
+  alertTitle = '';
+  alertMessage = '';
 
   constructor() {}
 
