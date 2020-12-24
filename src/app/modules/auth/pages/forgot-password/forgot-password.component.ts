2eb26a12f65ea0fef6fc9d233556b2ae7b2c5000
Junan Chakma <junan@nimblehq.co>
2020-12-24 10:14:00 +0700

    Added forgot password component

--- src/app/modules/auth/pages/forgot-password/forgot-password.component.ts
+++ src/app/modules/auth/pages/forgot-password/forgot-password.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-forgot-password',
-  templateUrl: './forgot-password.component.html'
+  templateUrl: './forgot-password.component.html',
 })
 export class ForgotPasswordComponent implements OnInit {
   constructor() {}
