fafb73a37897b8bab303557deb9b7cd88e5f2973
Junan Chakma <junan@nimblehq.co>
2020-12-24 10:46:44 +0700

    Make DRY

--- src/app/modules/auth/pages/sign-in/sign-in.component.ts
+++ src/app/modules/auth/pages/sign-in/sign-in.component.ts
@@ -3,9 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
-  styleUrls: ['./sign-in.component.scss']
+  styleUrls: ['./sign-in.component.scss'],
 })
 export class SignInComponent implements OnInit {
+  headerSubTitle = 'Sign in to Nimble';
   constructor() {}
 
   ngOnInit(): void {}
