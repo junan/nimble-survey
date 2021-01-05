2ee6c40e4305648d0ed71858d94cdbc69d5d4860
Junan Chakma <junan@nimblehq.co>
2021-01-05 14:44:00 +0700

    Update the key name

--- src/app/shared/success-messages.ts
+++ src/app/shared/success-messages.ts
@@ -1,4 +1,4 @@
 export const successMessages = {
-  FORGOT_PASSWORD_MESSAGE: $localize`:@@forgetPasswordMessage:We’ve email you instructions to reset your password.`,
-  FORGOT_PASSWORD_TITLE: $localize`:@@forgetPasswordTitle:Check your email`,
+  FORGOT_PASSWORD_MESSAGE: $localize`:@@forgotPasswordMessage:We’ve email you instructions to reset your password.`,
+  FORGOT_PASSWORD_TITLE: $localize`:@@forgotPasswordTitle:Check your email`,
 };
