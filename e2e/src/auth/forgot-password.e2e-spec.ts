620b06cb03f20c19a5778bda8eddf7d7f9ccff2c
Junan Chakma <junan@nimblehq.co>
2021-01-05 16:39:48 +0700

    dded forgot password spec

--- e2e/src/auth/forgot-password.e2e-spec.ts
+++ e2e/src/auth/forgot-password.e2e-spec.ts
@@ -1,41 +1,48 @@
 import { browser, until } from 'protractor';
-import { SignInPage } from './sign-in.po';
+import { ForgotPasswordPage } from './forgot-password.po';
 
-describe('SignIn', () => {
-  let page: SignInPage;
+describe('ForgotPassword', () => {
+  let page: ForgotPasswordPage;
 
   beforeEach(async () => {
-    page = new SignInPage();
+    page = new ForgotPasswordPage();
     await page.navigateTo();
   });
 
-  describe('Given valid credentials', () => {
+  describe('Given valid oauth credentials', () => {
     it('enables the submit button', async () => {
       await page.fillIn(page.emailInput(), 'john@example.com');
-      await page.fillIn(page.passwordInput(), 'secret');
 
       expect(await page.submitButton().getAttribute('disabled')).toBeFalsy();
     });
 
-    it('redirects to the root path after sign in', async () => {
-      await page.signIn('john@example.com', 'secret');
+    it('displays the error message', async () => {
+      await page.forgotPassword('john@example.com');
 
-      browser.wait(() => {
-        return until.urlIs('/');
-      });
+      browser
+        .wait(() => {
+          return until.elementIsVisible(page.alert());
+        })
+        .then(() => {
+          browser.wait(() => {
+            return until.elementTextIs(
+              page.alert(),
+              'We’ve email you instructions to reset your password.'
+            );
+          });
+        });
     });
   });
 
-  describe('Given invalid credentials', () => {
+  describe('Given invalid oauth credentials', () => {
     it('disables the submit button', async () => {
       await page.fillIn(page.emailInput(), '');
-      await page.fillIn(page.passwordInput(), 'secret');
 
       expect(await page.submitButton().getAttribute('disabled')).toBeTruthy();
     });
 
     it('displays the error message', async () => {
-      await page.signIn('invalid@email.com', 'secret');
+      await page.forgotPassword('john@example.com');
 
       browser
         .wait(() => {
