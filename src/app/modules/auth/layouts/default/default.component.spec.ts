a7664db48e8d083cf18125266827a37b8045ab86
Junan Chakma <junan@nimblehq.co>
2021-01-11 15:13:48 +0700

    Fixed lint error

--- src/app/modules/auth/layouts/default/default.component.spec.ts
+++ src/app/modules/auth/layouts/default/default.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { HttpClientModule } from "@angular/common/http";
-import { RouterTestingModule } from "@angular/router/testing";
+import { HttpClientModule } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { DefaultComponent } from './default.component';
 
