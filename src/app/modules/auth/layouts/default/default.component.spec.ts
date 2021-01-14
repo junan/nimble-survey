c5caca1ad74d9f4e1b56044df5aaef0396470c1c
Junan Chakma <junan@nimblehq.co>
2021-01-14 15:25:47 +0700

    Added spec

--- src/app/modules/auth/layouts/default/default.component.spec.ts
+++ src/app/modules/auth/layouts/default/default.component.spec.ts
@@ -3,26 +3,39 @@ import { HttpClientModule } from '@angular/common/http';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { DefaultComponent } from './default.component';
+import { AuthHeaderComponent } from '@component/auth/components/auth-header/auth-header.component';
 
 describe('DefaultComponent', () => {
   let component: DefaultComponent;
   let fixture: ComponentFixture<DefaultComponent>;
+  let baseElement: any;
+
+  const SELECTORS = {
+    headerTitle: '.auth-header__title',
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [DefaultComponent],
+      declarations: [DefaultComponent, AuthHeaderComponent],
       imports: [HttpClientModule, RouterTestingModule.withRoutes([])],
-    })
-      .compileComponents();
+    }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(DefaultComponent);
     component = fixture.componentInstance;
+    component.headerTitle = 'Sign in to Nimble';
+    baseElement = fixture.nativeElement;
     fixture.detectChanges();
   });
 
   it('creates the component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('renders header title', () => {
+    expect(
+      baseElement.querySelector(SELECTORS.headerTitle).textContent
+    ).toContain('Sign in to Nimble');
   });
 });
